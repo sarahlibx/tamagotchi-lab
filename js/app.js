361ee4b317e5ea927953cb1ee7c49a6c2464fe64
Sarah Smith <sarahlib90@gmail.com>
2025-11-13 19:29:39 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,76 @@
+// Display three buttons when the page is initially displayed.
+// Display three stats that randomly increment a value between 0 and 3 at a set interval.
+// To keep the creature happy, a player must keep three stats within a given range - greater than 0 and less than 10.
+// A player can click a button to set the corresponding stat to 0.
+// Include loss logic and display a message when the player has lost.
+// Provide a Play Again button that will reset the game.
+
+// 1) Define the required variables used to track the state of the game.
+
+// 2) Store cached element references.
+
+// 3) Upon loading, the game state should be initialized, and a function should be called to render this game state.
+
+// 4) The state of the game should be rendered to the user.
+
+// 5) Handle the game over logic. 
+
+// 6) Handle each instance of a player clicking a button with the use of a `handleClick()` function.
+
+// 7) Create reset functionality.
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+const object = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0
+};
+
+//store game timer
+let timer;
+
+//present if player has lost
+let gameOver;
+
+
+/*------------------------ Cached Element References ------------------------*/
+// store spans inside the stat-wrapper
+const boredomStatEl = document.querySelector('#boredom-stat');
+const hungetStatEl = document.querySelector('#hunger-stat');
+const sleepinessStatEl = document.querySelector('#sleepiness-stat');
+
+// store button elements inside of game interface section
+const playBtnEl = document.querySelector('#play');
+const feedBtnEl = document.querySelector('#feed');
+const sleepBtnEl = document.querySelector('#sleep');
+
+// game status
+const gameMessageEl = document.querySelector('.game-state-wrapper');
+
+// play again
+const resetBtnEl = document.querySelector('#restart');
+
+/*-------------------------------- Functions --------------------------------*/
+// game state initialization
+const init = () => {
+    gameOver = false;
+    timer = setInterval(function() {
+        runGame();
+    }, 2000);
+}
+
+const runGame = () => {
+    // console.log('the game is running!');
+    // renderGame();
+};
+
+init();
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
